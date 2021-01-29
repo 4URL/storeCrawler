4911884083a522828bbf91e1b660725ae8ae934c
bckang0816 <67310514+bckang0816@users.noreply.github.com>
2021-01-29 11:53:52 +0900

    Initial Init

---
+++ src/test/java/aa/store/crawler/CrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package aa.store.crawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
